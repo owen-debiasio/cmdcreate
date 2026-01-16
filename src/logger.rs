2b7d85a52edb6f37917da8d0192cd535b56f142f
Owen Debiasio <owen.debiasio@gmail.com>
2026-01-16 15:24:54 -0500

    Added the ability to log/print verbose info

---
+++ src/logger.rs
@@ -0,0 +1,31 @@
+use crate::{
+    PATHS,
+    configs::load,
+    utils::{colors::COLORS, fs::write_to_file, sys::args_contains},
+};
+use chrono::Local;
+
+pub fn log(text: &str, lvl: u8) {
+    let time = Local::now()
+        .format(&load("logs", "time_format", "%Y-%m-%d %H:%M:%S"))
+        .to_string();
+
+    let color = match lvl {
+        0 => COLORS.cyan,
+        1 => COLORS.yellow,
+        2 => COLORS.red,
+        _ => COLORS.reset,
+    };
+
+    if load("logs", "verbose", "").parse::<bool>().unwrap_or(false)
+        || args_contains("-V")
+        || args_contains("--verbose")
+    {
+        println!("{color}{text}{}", COLORS.reset);
+    }
+
+    write_to_file(
+        &format!("{}/{time}.log", PATHS.log_dir),
+        &format!("{time} {text}\n"),
+    );
+}
