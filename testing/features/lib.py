b85966d55c7f8ebc12796707cfd571675c2f23fc
Owen Debiasio <odebiasio2029@proton.me>
2026-01-29 08:21:15 -0500

    Suppress docstring errors

--- testing/features/lib.py
+++ testing/features/lib.py
@@ -1,13 +1,8 @@
-"""Stores system utilities used by the script"""
-
 import subprocess
 import os
 
 
 def command(cmd):
-    """Function to run shell commands"""
     subprocess.run(cmd, shell=True, check=True)
 
-
-"""Retrieve the home directory"""
 home = os.path.expanduser("~")
