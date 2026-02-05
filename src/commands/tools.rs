1148cd25cdf3dd1534812bf7dbce4b213017f53b
Owen <owen.debiasio@gmail.com>
2026-02-05 16:28:37 -0500

    Removed unused code and code optimizations

--- src/commands/tools.rs
+++ src/commands/tools.rs
@@ -25,39 +25,17 @@ pub fn command_is_installed(cmd: &str) -> bool {
             0,
         );
 
-        true
-    } else {
-        log(
-            &format!(
-                "commands/tools::command_is_installed(): Command \"{cmd}\" is not installed... Exiting..."
-            ),
-            0,
-        );
-
-        error(&format!("Command \"{cmd}\" is not installed"), "");
-    }
-}
-
-pub fn _is_command_installed(cmd: &str) -> bool {
-    log(
-        &format!(
-            "commands/tools::is_command_installed(): Checking if command \"{cmd}\" is installed..."
-        ),
-        0,
-    );
-
-    if !path_exists(&format!("{}/{cmd}", PATHS.install_dir)) {
-        error(&format!("Command \"{cmd}\" is not installed"), "");
+        return true;
     }
 
     log(
         &format!(
-            "commands/tools::is_command_installed(): Command \"{cmd}\" is installed, continuing..."
+            "commands/tools::command_is_installed(): Command \"{cmd}\" is not installed... Exiting..."
         ),
         0,
     );
 
-    true
+    error(&format!("Command \"{cmd}\" is not installed"), "");
 }
 
 pub fn get_installed_commands() -> Vec<PathBuf> {
