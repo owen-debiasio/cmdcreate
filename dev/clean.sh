86776145d021e668c03bde9fa6bdd301e5275c17
Owen <owen.debiasio@gmail.com>
2026-03-04 07:59:47 -0500

    fixed paths

--- dev/clean.sh
+++ dev/clean.sh
@@ -23,8 +23,8 @@ cargo clean
 
 echo -e "\n> Cleaning up Python cache files..."
 
-rm -rf ./testing/features/__pycache__/
-rm -rf ./testing/features/scripts/__pycache__/
+rm -rf ./testing/__pycache__/
+rm -rf ./testing/scripts/__pycache__/
 
 ./dev/format.sh
 
