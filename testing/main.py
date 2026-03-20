91d516f9b17da1baf30bde397363a6d5b5b03436
Owen Debiasio <owen.debiasio@gmail.com>
2026-03-20 10:15:20 -0400

    Add TODO
    todo: refactor and rewrite testing scripts

--- testing/main.py
+++ testing/main.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# TODO: Refactor + rewrite testing libraries
+
 from lib import command
 from scripts import (
     remove,
