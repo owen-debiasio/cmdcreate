a534768a54f39b0903c1adddfcf8f836cb9d3cdc
Owen Debiasio <owen.debiasio@gmail.com>
2026-04-03 11:26:54 -0400

    fix typo

--- src/utils/fs.rs
+++ src/utils/fs.rs
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 //! This library just provides wrappers to already existing functions!
-//! They are technically useless but they make more sense to me.
+//! They are technically useless, but they make more sense to me.
 //! If using filesystem actions, please refer to here.
 
 use crate::{
