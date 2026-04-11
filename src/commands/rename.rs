1fc993af3efb53ea9b76cfa60af8c98236a4665c
Owen Debiasio <owen.debiasio@gmail.com>
2026-04-10 20:57:24 -0400

    Now handles favorite command status properly

--- src/commands/rename.rs
+++ src/commands/rename.rs
@@ -16,6 +16,7 @@
 
 use crate::{
     commands::{
+        favorite::{command_is_in_favorites, favorite},
         remove::remove,
         tools::{cmdcreate_command_is_installed, get_installed_commands},
     },
@@ -66,28 +67,47 @@ pub fn rename(old_command_name: &str, new_renamed_command_name: &str) {
 
     log(
         &format!(
-            "commands/rename::rename(): Renaming command \"{old_command_name}\" to \"{new_renamed_command_name}\"..."
+            "commands/rename::rename(): Renaming command \"{old_command_name}\" \
+            to \"{new_renamed_command_name}\"..."
         ),
         Severity::Normal,
     );
 
+    let mut check_favorites_in_repair: bool = false;
+    if command_is_in_favorites(old_command_name) {
+        check_favorites_in_repair = true;
+
+        favorite("remove", old_command_name);
+        favorite("add", new_renamed_command_name);
+    }
+
     // I should probably make a function to move files
     run_shell_command(&format!(
-        "mv {command_install_location}{old_command_name} {command_install_location}{new_renamed_command_name}"
+        "mv \
+        {command_install_location}{old_command_name} \
+        {command_install_location}{new_renamed_command_name}"
     ));
 
     command_rename_success(
         old_command_name,
         new_renamed_command_name,
         command_install_location,
+        check_favorites_in_repair,
     );
 
     println!(
-        "{green}Successfully renamed command {blue}\"{old_command_name}\" to {blue}\"{new_renamed_command_name}\"{reset}"
+        "{green}Successfully renamed command \
+        {blue}\"{old_command_name}\" \
+        to {blue}\"{new_renamed_command_name}\"{reset}"
     );
 }
 
-fn command_rename_success(_old_name: &str, new_name: &str, path_of_old_command: &str) {
+fn command_rename_success(
+    old_name: &str,
+    new_name: &str,
+    path_of_old_command: &str,
+    check_favorite_status: bool,
+) {
     log(
         "commands/rename::command_rename_success(): \
         Determining command creation status...",
@@ -103,6 +123,22 @@ fn command_rename_success(_old_name: &str, new_name: &str, path_of_old_command: 
 
     if !cmdcreate_command_is_installed(new_name) {
         error("Failed to rename command!", "New command does not exist.");
+    }
+
+    if check_favorite_status {
+        if command_is_in_favorites(old_name) {
+            error(
+                "Failed to rename command!",
+                "Old command still remains in favorites.",
+            );
+        }
+
+        if !command_is_in_favorites(new_name) {
+            error(
+                "Failed to rename command!",
+                "New command is not in the favorites file.",
+            );
+        }
     }
 
     log(
