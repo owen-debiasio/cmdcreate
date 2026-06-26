787a4c0ea7317372d5a2c15d3a00ac1e67a747ed
Owen Debiasio <owen.debiasio@gmail.com>
2026-06-26 19:35:34 -0400

    New developer utility, tests cmdcreate using `cargo test`

---
+++ dev/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2026 Owen Debiasio
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -euo pipefail
+
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+echo -e "\n${BLUE}> Running tests...${RESET}"
+
+cargo test --release
+
+# Tests that have to be run separately
+cargo test --release export_file_contains_command_with_contents -- --ignored
+cargo test --release export_file_is_created -- --ignored
+
+echo -e "\n${GREEN}> Finished tests!${RESET}"
