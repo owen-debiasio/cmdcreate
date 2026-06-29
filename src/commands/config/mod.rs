8480987d1bdd7b09a202a6849aaf0ea33cbf5db6
Owen Debiasio <owen.debiasio@gmail.com>
2026-06-29 15:08:56 -0400

    Organized files

--- src/commands/config/mod.rs
+++ src/commands/config/mod.rs
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod config_add;
-mod config_remove;
 pub mod main;
+
+mod actions;
+mod list;
+
+mod verify;
